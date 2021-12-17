c721c8da16bd106578c2da7cd6943013a6d9323a
M. Dietrich <mdt@emdete.de>
2021-12-17 11:21:36 +0100

    rename ascii to asciiart (name clash)

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import os
 import torch
 from Filter.jpeg_artifacts import jpeg_corruption, jpeg_compression
 from Filter.animalfilter import animalfilter
-from Filter.Ascii import ascii
+from Filter.asciiart import asciiart
 from Filter.binary import binary
 from Filter.blurfilter import blurfilter
 from Filter.edgeFilter import edgeFilter
@@ -18,28 +18,37 @@ filters = dict(
 	animalfilter=animalfilter,
 	jpeg_compression=jpeg_compression,
 	jpeg_corruption=jpeg_corruption,
-	ascii=ascii,
+	asciiart=asciiart,
 	binary=binary,
 	blurfilter=blurfilter,
 	edgeFilter=edgeFilter,
 	pixelError=pixelError,
 	waterfilter=waterfilter,
 	)
+further = True
 
 def run(state):
+	global filters, model, further
 	capture = cv.VideoCapture(os.environ["WEBCAM_INDEX"] if "WEBCAM_INDEX" in os.environ else 0)
-	fmt = pyvirtualcam.PixelFormat.BGR
 	width = int(capture.get(cv.CAP_PROP_FRAME_WIDTH))
 	height = int(capture.get(cv.CAP_PROP_FRAME_HEIGHT))
-
-	with pyvirtualcam.Camera(width, height, 20, fmt=fmt, device="/dev/video2") as cam:
+	with pyvirtualcam.Camera(
+		width=width,
+		height=height,
+		fps=20,
+		fmt=pyvirtualcam.PixelFormat.BGR,
+		device="/dev/video2",
+		backend='v4l2loopback',
+	) as cam:
 		print(f'Using virtual camera: {cam.device}')
-		while True:
+		while further:
 			isTrue, frame = capture.read()
 			frame = np.flip(frame, axis=1)
 			frame = loop(frame, state.recording, state.glitch)
-			frame = filters.get(state.filter, lambda f:f)(frame, model=model, width=width, height=height)
+			if state.filter in filters:
+				frame = filters[state.filter](frame, model=model, width=width, height=height)
 			cam.send(frame)
 			cam.sleep_until_next_frame()
 	capture.release()
 	cv.destroyAllWindows()
+
